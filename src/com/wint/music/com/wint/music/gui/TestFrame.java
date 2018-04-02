d27f90dff15c409bd9b223424b172f3d81d19bf8
LAPTOP-K41JB88F\peter <peterkoziman>
2018-04-02 21:09:38 +0200

    Button to change a label and button to change color of oval using inner classes as ActionListener

--- src/com/wint/music/com/wint/music/gui/TestFrame.java
+++ src/com/wint/music/com/wint/music/gui/TestFrame.java
@@ -7,18 +7,25 @@ import java.awt.event.ActionListener;
 
 public class TestFrame implements ActionListener {
     private JFrame frame;
+    private JLabel label;
 
     public TestFrame() {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton button = new JButton("Change color");
-        button.addActionListener(this);
+        JButton labelButt = new JButton("Change label");
+        labelButt.addActionListener(new LabelListener());
 
+        JButton colorButt = new JButton("Change color");
+        colorButt.addActionListener(new ColorListener());
+
+        label = new JLabel("Label");
         MyDrawPanel p = new MyDrawPanel();
 
         frame.getContentPane().add(BorderLayout.CENTER, p);
-        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.EAST, labelButt);
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButt);
+        frame.getContentPane().add(BorderLayout.WEST, label);
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
@@ -26,5 +33,19 @@ public class TestFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         frame.repaint();
+    }
+
+    private class LabelListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            label.setText(label.getText() + "\nd'oh!");
+        }
+    }
+
+    private class ColorListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.repaint();
+        }
     }
 }
