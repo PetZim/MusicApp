d212a9288c965c7007046883fb56c4f2b5cb5036
LAPTOP-K41JB88F\peter <peterkoziman>
2018-04-02 20:09:44 +0200

    no message

--- src/com/wint/music/Main.java
+++ src/com/wint/music/Main.java
@@ -1,10 +1,11 @@
 package com.wint.music;
 
 import com.wint.music.com.wint.music.gui.SimpleGuiB;
+import com.wint.music.com.wint.music.gui.TestFrame;
 
 public class Main {
 
     public static void main(String[] args) {
-        new SimpleGuiB();
+        new TestFrame();
     }
 }
