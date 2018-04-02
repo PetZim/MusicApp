4b0f5e477265553196f79f6027d8856469435196
LAPTOP-K41JB88F\peter <peterkoziman>
2018-04-02 20:33:22 +0200

    Graphics2d fill oval with gradient

--- src/com/wint/music/com/wint/music/gui/MyDrawPanel.java
+++ src/com/wint/music/com/wint/music/gui/MyDrawPanel.java
@@ -6,7 +6,10 @@ import java.awt.*;
 public class MyDrawPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
-        g.setColor(Color.orange);
-        g.fillRect(20, 50, 100, 100);
+        Graphics2D g2d = (Graphics2D) g;
+
+        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
     }
 }
