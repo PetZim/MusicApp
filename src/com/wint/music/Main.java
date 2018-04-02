12ef37a2fc0cf099b8c4bb421e54d210b2395324
LAPTOP-K41JB88F\peter <peterkoziman>
2018-04-02 18:37:08 +0200

    Play a single note on piano.

---
+++ src/com/wint/music/Main.java
@@ -0,0 +1,9 @@
+package com.wint.music;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+        mini.play();
+    }
+}
---
+++ src/com/wint/music/MiniMiniMusicApp.java
@@ -0,0 +1,29 @@
+package com.wint.music;
+
+import javax.sound.midi.*;
+
+public class MiniMiniMusicApp {
+    public void play() {
+        try {
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            Sequence sequence = new Sequence(Sequence.PPQ,4);
+            Track track = sequence.createTrack();
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144,1,44,100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128,1,44,100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+
+            player.setSequence(sequence);
+            player.start();
+        } catch (InvalidMidiDataException | MidiUnavailableException e) {
+            e.printStackTrace();
+            System.err.println("Which exception: " + e.getClass());
+        }
+    }
+}
