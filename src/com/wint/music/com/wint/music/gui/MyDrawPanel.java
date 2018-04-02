4b4cd47d0eb7b0f4d437498d18aa761c6bce4645
LAPTOP-K41JB88F\peter <peterkoziman>
2018-04-02 20:45:25 +0200

    Repaint frame when button is clicked

--- src/com/wint/music/com/wint/music/gui/MyDrawPanel.java
+++ src/com/wint/music/com/wint/music/gui/MyDrawPanel.java
@@ -5,11 +5,17 @@ import java.awt.*;
 
 public class MyDrawPanel extends JPanel {
     @Override
-    public void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-
-        GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+    public void paintComponent(Graphics gr) {
+        Graphics2D g2d = (Graphics2D) gr;
+        GradientPaint gradient = new GradientPaint(70, 70, getRandomColor(), 150, 150, getRandomColor());
         g2d.setPaint(gradient);
         g2d.fillOval(70, 70, 100, 100);
+    }
+
+    private Color getRandomColor() {
+        int r = (int) (Math.random() * 255);
+        int g = (int) (Math.random() * 255);
+        int b = (int) (Math.random() * 255);
+        return new Color(r, g, b);
     }
 }
--- src/com/wint/music/com/wint/music/gui/TestFrame.java
+++ src/com/wint/music/com/wint/music/gui/TestFrame.java
@@ -1,15 +1,30 @@
 package com.wint.music.com.wint.music.gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TestFrame {
+public class TestFrame implements ActionListener {
+    private JFrame frame;
 
     public TestFrame() {
-        JFrame frame = new JFrame();
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Change color");
+        button.addActionListener(this);
+
         MyDrawPanel p = new MyDrawPanel();
-        frame.getContentPane().add(p);
+
+        frame.getContentPane().add(BorderLayout.CENTER, p);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        frame.repaint();
+    }
 }
